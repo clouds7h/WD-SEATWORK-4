0184f332be533852e36e645089e9505d94b472bb
HERNANDEZ_CLODETE <202312370@fit.edu.ph>
2024-06-25 13:16:02 +0800

    -Fixed the design of all Hero sections.

---
+++ pages/page1/assets/js/scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menu_box = document.querySelector(".menu-li");
+            const navbar = document.querySelector(".navbar");
+            navbar.classList.toggle('active');
+            menu_box.classList.toggle('active');
+    });
